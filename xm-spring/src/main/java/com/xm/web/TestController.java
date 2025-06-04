2aec521902392ef65af53ef9e69eef7bd7359647
xm <xmaaaa66@gmail.com>
2025-06-04 19:33:36 +0800

    fix

--- xm-spring/src/main/java/com/xm/web/TestController.java
+++ xm-spring/src/main/java/com/xm/web/TestController.java
@@ -3,8 +3,6 @@ package com.xm.web;
 import com.xm.scope.RequestBean;
 import com.xm.scope.SessionBean;
 import com.xm.service.TestService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
-    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
     @Autowired
     private TestService testService;
