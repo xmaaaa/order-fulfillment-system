f682947d10c31bc027e9ddf5b31bcc23dc9611d5
xm <xmaaaa66@gmail.com>
2025-09-08 23:04:33 +0800

    add

--- xm-spring/src/main/java/com/xm/util/MyServiceFactoryBean.java
+++ xm-spring/src/main/java/com/xm/util/MyServiceFactoryBean.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.FactoryBean;
 
 /**
  * FactoryBean 实现，用于自定义 MyService 的创建
+ * 通过 FactoryBean 可以更自由的创建bean对象，spring先把factoryBean 放入bean，再把getObject的放入bean
+ * 如果我自己想把代理对象或者复杂逻辑加入到bean中，我就需要自己管理创建bean逻辑，并且注入到容器，依赖等等，很复杂
+ *
  *
  * @author xm
  */
