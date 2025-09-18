3446855c03969abffd99ec48afa085e9fe57ffe7
xm <xmaaaa66@gmail.com>
2025-09-18 09:33:25 +0800

    add

--- xm-spring/src/main/java/com/xm/aop/LoggerAspect.java
+++ xm-spring/src/main/java/com/xm/aop/LoggerAspect.java
@@ -12,10 +12,12 @@ import org.springframework.stereotype.Component;
 /**
  * 日志切面Aspect，把一个或多个通知（Advice）+ 一个或多个切入点（Pointcut）组合起来
  * Weaving（织入）: 就是把切面代码插入到目标方法中的过程。默认使用 动态代理-- JDK Proxy（接口）或 CGLIB（无接口）
- *
+ * 目标对象（Target Object） 是你要增强的原始类。
+ * 代理对象（Proxy） 是实际运行时被调用的对象
+ * --
  * Spring AOP 的底层是通过 {@link AnnotationAwareAspectJAutoProxyCreator} 这个 BeanPostProcessor 实现的。
  * 在 bean 初始化完成后，它会判断是否匹配切点，如果匹配，就用 JDK 动态代理或 CGLIB 生成代理对象。
- * 方法调用时走到代理逻辑，通过 {@link ReflectiveMethodInvocation} 维护一个拦截器链，把各种通知（@Before、@Around、@After 等）按顺序织入，最后再调用目标方法。
+ * 方法调用时走到代理逻辑，通过 {@link ReflectiveMethodInvocation} 维护一个拦截器链，把各种通知按顺序织入，最后再调用目标方法。
  *
  * @author XM
  * @date 2023/1/17
@@ -44,6 +46,11 @@ public class LoggerAspect {
         System.out.println("before");
     }
 
+    /**
+     * After Returning（后置通知）：方法正常返回后运行
+     * After Throwing（异常通知）：方法抛异常时运行
+     * After (Finally)（最终通知）：不管方法是否抛异常都会运行
+     */
     @After("log()")
     public void doAfter() {
         // .......
