b610544fdd9f1e7f7c1d945429f3a86a58041dfd
xm <xmaaaa66@gmail.com>
2025-09-23 20:53:13 +0800

    add

--- xm-base/src/main/java/com/xm/designpattern/behavior/iterator/NameRepository.java
+++ xm-base/src/main/java/com/xm/designpattern/behavior/iterator/NameRepository.java
@@ -5,31 +5,28 @@ package com.xm.designpattern.behavior.iterator;
  * @date 2023/1/11
  */
 public class NameRepository implements Container {
-   public String[] names = {"Robert" , "John" ,"Julie" , "Lora"};
- 
-   @Override
-   public Iterator getIterator() {
-      return new NameIterator();
-   }
- 
-   private class NameIterator implements Iterator {
- 
-      int index;
- 
-      @Override
-      public boolean hasNext() {
-         if(index < names.length){
-            return true;
-         }
-         return false;
-      }
- 
-      @Override
-      public Object next() {
-         if(this.hasNext()){
-            return names[index++];
-         }
-         return null;
-      }     
-   }
+    public String[] names = {"Robert", "John", "Julie", "Lora"};
+
+    @Override
+    public Iterator getIterator() {
+        return new NameIterator();
+    }
+
+    private class NameIterator implements Iterator {
+
+        int index;
+
+        @Override
+        public boolean hasNext() {
+            return index < names.length;
+        }
+
+        @Override
+        public Object next() {
+            if (this.hasNext()) {
+                return names[index++];
+            }
+            return null;
+        }
+    }
 }
