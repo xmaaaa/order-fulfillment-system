c64975d3e380b8c76f9124bb68e8044aab51256f
xm <xmaaaa66@gmail.com>
2025-09-25 17:37:29 +0800

    add

---
+++ xm-base/src/main/java/com/xm/designpattern/behavior/command/Net.java
@@ -0,0 +1,16 @@
+package com.xm.designpattern.behavior.command;
+
+/**
+ * @author XM
+ * @date 2025/9/10
+ */
+public class Net {
+
+    public void on() {
+        System.out.println("Net on");
+    }
+
+    public void off() {
+        System.out.println("Net off");
+    }
+}
