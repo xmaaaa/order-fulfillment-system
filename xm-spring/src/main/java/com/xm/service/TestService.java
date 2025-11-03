90f2a81f0f53d22fd97d865d1fdcb837258d33a9
xm <xmaaaa66@gmail.com>
2025-11-03 18:28:04 +0800

    fix

--- xm-spring/src/main/java/com/xm/service/TestService.java
+++ xm-spring/src/main/java/com/xm/service/TestService.java
@@ -1,12 +1,22 @@
 package com.xm.service;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+
 /**
  * @author hongwan
  * @date 2023/1/17
  */
 public class TestService {
 
+    @SentinelResource(value = "test", blockHandler = "handleBlock")
     public String test() {
+        System.out.println("test");
         return "test";
+    }
+
+
+    public String handleBlock(BlockException ex) {
+        return "系统繁忙，请稍后再试";
     }
 }
