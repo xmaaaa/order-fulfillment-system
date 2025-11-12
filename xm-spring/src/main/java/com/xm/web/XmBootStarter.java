e9a9908b4712343a8758cf6e53707f795b12f3ed
xm <xmaaaa66@gmail.com>
2025-11-12 11:12:16 +0800

    fix

--- xm-spring/src/main/java/com/xm/web/XmBootStarter.java
+++ xm-spring/src/main/java/com/xm/web/XmBootStarter.java
@@ -1,5 +1,6 @@
 package com.xm.web;
 
+import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
  * @author hongwan
  * @date 2023/1/17
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedissonAutoConfiguration.class})
 @ComponentScan(basePackages = {
         "com.xm"
 })
