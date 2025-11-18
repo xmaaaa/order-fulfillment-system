505b0c53255057d15643fc97e92c6d848175619e
xm <xmaaaa66@gmail.com>
2025-11-18 09:00:14 +0800

    fix

--- xm-base/src/main/java/com/xm/sort/QuickSort.java
+++ xm-base/src/main/java/com/xm/sort/QuickSort.java
@@ -92,7 +92,7 @@ public class QuickSort {
         return left;
     }
 
-    public static void swap(int[] a, int i, int j) {
+    private static void swap(int[] a, int i, int j) {
         int temp = a[i];
         a[i] = a[j];
         a[j] = temp;
